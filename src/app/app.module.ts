e24a816ae336e9d8aabd24bd0e14b2b58e94cbd0
Arlene Salgado <asalgado@storage-c02q241mf8j4.hnsc.net>
2018-12-10 16:46:00 -0600

    header and section1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { Section1Component } from './section1/section1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    Section1Component
   ],
   imports: [
     BrowserModule,
